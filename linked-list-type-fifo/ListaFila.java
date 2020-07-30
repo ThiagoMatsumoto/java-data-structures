80351afbc02ce3e2f1a56c554c29107037bd17dc
Thiago Matsumoto SIqui <50118104+ThiagoMatsumoto@users.noreply.github.com>
2020-07-30 17:32:36 -0300

    Create ListaFila.java

---
+++ linked-list-type-fifo/ListaFila.java
@@ -0,0 +1,36 @@
+package linked-list-type-fifo;
+
+/**
+ * FIFO - First In First Out
+ */
+public class ListaFila {
+    No inicio;
+    No fim;
+    public ListaFila(){
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    public void insere(int num){
+        if(this.inicio == null){
+            this.fim = new No(num);
+            this.inicio = this.fim;
+        } else {
+            this.fim.prox = new No(num);
+            this.fim = fim.prox;
+        }
+    }
+
+    public void imprimir(){
+        No p = this.inicio;
+        while(p != this.fim){
+            System.out.println(p.num);
+            p = p.prox;
+            if (p == this.fim) System.out.println(p.num);
+        }
+    }
+
+    public void remover(){
+        this.inicio = this.inicio.prox;
+    }
+}
