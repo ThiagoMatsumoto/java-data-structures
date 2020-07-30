fa7c6a9ccd4d756e540293f60592985204d2fc6c
Thiago Matsumoto SIqui <50118104+ThiagoMatsumoto@users.noreply.github.com>
2020-07-30 17:34:46 -0300

    Create Teste.java

---
+++ linked-list-type-fifo/Teste.java
@@ -0,0 +1,19 @@
+package linked-list-type-fifo;
+
+public class Teste {
+    public static void main(String[] args){
+        ListaFila lf = new ListaFila();
+        lf.insere(1);
+        lf.insere(2);
+        lf.insere(3);
+        lf.insere(4);
+
+        lf.imprimir();
+        System.out.println();
+        lf.remover();
+        lf.imprimir();
+        System.out.println();
+        lf.remover();
+        lf.imprimir();
+    }
+}
