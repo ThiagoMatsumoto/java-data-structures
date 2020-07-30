dc62c722856194306ed763d2221bd47259700c01
Thiago Matsumoto SIqui <50118104+ThiagoMatsumoto@users.noreply.github.com>
2020-07-30 14:06:20 -0300

    Create Teste.java

---
+++ circular-linked-list/Teste.java
@@ -0,0 +1,27 @@
+package circular-linked-list;
+
+import java.util.Scanner;
+
+public class Teste {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        ListaCircular lc = new ListaCircular();
+
+        System.out.println("Quantos valores deseja inserir?");
+        int n = scanner.nextInt();
+        System.out.println("Digite os valores que deseja inserir:");
+
+        for(int i = 0; i < n; i++){
+            int valor = scanner.nextInt();
+            lc.inserir(valor);
+        }
+        scanner.close();
+        
+        System.out.println();
+        lc.imprime();
+
+        lc.inverte();
+        System.out.println();
+        lc.imprime();
+    }
+}
